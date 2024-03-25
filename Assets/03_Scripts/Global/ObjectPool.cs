bacb010d6ed20d6e867cc05fecfaea0fea801634
s8st <153998744+s8st@users.noreply.github.com>
2024-03-25 12:05:20 +0900

    Feat_오브젝트 풀, Bomb 발사체

---
+++ Assets/03_Scripts/Global/ObjectPool.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    [System.Serializable]
+    public struct Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    private void Awake()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach (var pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag)
+    {
+        if (!poolDictionary.ContainsKey(tag))
+            return null;
+
+        GameObject obj = poolDictionary[tag].Dequeue();
+        poolDictionary[tag].Enqueue(obj);
+        // 맨 앞의 것을 빼서 맨 뒤로 , 즉 제한 갯수를 넘어서면 뒤의 것을 계속 사용??
+
+
+        return obj;
+    }
+}
--- Assets/03_Scripts/Global/ProjectileManager.cs
+++ Assets/03_Scripts/Global/ProjectileManager.cs
@@ -6,10 +6,14 @@ public class ProjectileManager : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _impactParticleSystem;
 
-    public static ProjectileManager instance;
+    public static ProjectileManager instance; //싱글턴
 
-    [SerializeField] private GameObject testObj;
+    // [SerializeField] private GameObject testObj;
+    private ObjectPool objectPool;
 
+
+
+    // 싱글턴
     private void Awake()
     {
         instance = this;
@@ -17,18 +21,31 @@ public class ProjectileManager : MonoBehaviour
 
     void Start()
     {
-
+        objectPool = GetComponent<ObjectPool>();
     }
 
     public void ShootBullet(Vector2 startPostiion, Vector2 direction, RangedAttackData attackData)
     {
-        GameObject obj = Instantiate(testObj);
+        // GameObject obj = Instantiate(testObj);
+        GameObject obj = objectPool.SpawnFromPool(attackData.bulletNameTag);
 
         obj.transform.position = startPostiion;
         RangedAttackController attackController = obj.GetComponent<RangedAttackController>();
-        attackController.InitializeAttack(direction, attackData, this);
+        attackController.InitializeAttack(direction, attackData, this); // this == ProjectileManager
 
-        obj.SetActive(true);
+        obj.SetActive(true); // 재사용을 위해
     }
+
+    // 이 위치에 파티클
+    public void CreateImpactParticlesAtPostion(Vector3 position, RangedAttackData attackData)
+    {
+        _impactParticleSystem.transform.position = position; // 파티클 시스템 위치 변경
+        ParticleSystem.EmissionModule em = _impactParticleSystem.emission;
+        em.SetBurst(0, new ParticleSystem.Burst(0, Mathf.Ceil(attackData.size * 5)));
+        ParticleSystem.MainModule mainModule = _impactParticleSystem.main;
+        mainModule.startSpeedMultiplier = attackData.size * 10f;
+        _impactParticleSystem.Play();
+    }
+
 
 }
