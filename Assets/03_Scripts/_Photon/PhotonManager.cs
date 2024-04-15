051da1c176f00d480e578c8e0abcd44aff1b5ed6
s8st <153998744+s8st@users.noreply.github.com>
2024-04-15 15:55:23 +0900

    Build _ 포톤 테스트

--- Assets/03_Scripts/_Photon/PhotonManager.cs
+++ Assets/03_Scripts/_Photon/PhotonManager.cs
@@ -10,7 +10,7 @@ public class PhotonManager : /*MonoBehaviour*/ MonoBehaviourPunCallbacks
     private readonly string version = "1.0f";
 
     //사용자 아이디
-    private string userId = "Mary";
+    private string userId = "webglTester";
 
     private void Awake()
     {
@@ -41,9 +41,52 @@ public class PhotonManager : /*MonoBehaviour*/ MonoBehaviourPunCallbacks
     public override void OnJoinedLobby()
     {
         Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}"); //로비에 입장함으로 true, 
+        PhotonNetwork.JoinRandomRoom();//랜덤 메치메이킹 기능 제공
     }
 
+    //랜덤 방 입장 실패했을 때(방 없을 때) 콜백함수
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        // base.OnJoinRandomFailed(returnCode, message);
+        Debug.Log($"JoinRandom Failed {returnCode} : {message}"); //returnCode : 32760
+        //JoinRandom Failed 32760 : No match found
+        //UnityEngine.Debug:Log(object)
 
+        // 방 속성 정의
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 20; // 최대 플레이어
+        ro.IsOpen = true; // 방 개방
+        ro.IsVisible = true; //로비에서 방 공개
+
+        //방 생성
+        PhotonNetwork.CreateRoom("gogosing", ro);
+        //PhotonNetwork.CreateRoom("My Room", ro);
+
+    }
+
+    // 방 생성 후 호출되는 콜백함수
+    public override void OnCreatedRoom()
+    {
+        //base.OnCreatedRoom();
+        Debug.Log("방만들기 성공");
+        Debug.Log($"방 이름 : {PhotonNetwork.CurrentRoom.Name}");
+    }
+
+    // 방 입장 후 호출되는 콜백함수
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"PhotonNetwork.InRoom = {PhotonNetwork.InRoom}"); // true , false
+        Debug.Log($"Player Count = {PhotonNetwork.CurrentRoom.PlayerCount}"); // 방에 입장한 플레이어 수
+
+        // 방에 접속한 사용자 정보 확인
+        foreach(var player in PhotonNetwork.CurrentRoom.Players)
+        {
+            Debug.Log($"{player.Value.NickName},{player.Value.ActorNumber}"); //별명과 고유값, webglTester, 1
+            //$ == String.Format()
+        }
+
+
+    }
 
 
 
