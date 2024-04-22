33c8c41acede314d0c956a828c22c26a8979a675
s8st <153998744+s8st@users.noreply.github.com>
2024-04-22 11:48:07 +0900

    Fix_플레이어 선택 후 캠 추적하게 수정

---
+++ Assets/03_Scripts/Controllers/CamPos.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamPos : MonoBehaviour
+{
+    private GameObject playerObj;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerObj == null)
+        {
+            playerObj = GameObject.FindGameObjectWithTag("Player");
+
+        }
+        else
+        {
+            this.transform.position = new Vector3(playerObj.transform.position.x, playerObj.transform.position.y,-10f);
+        }
+
+        
+    }
+}
